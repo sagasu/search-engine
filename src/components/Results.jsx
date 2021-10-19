4d1ed16a6535c944b152cb9bd6bac30e56bac022
Mateusz Kopij <sagasu@gmail.com>
2021-10-19 21:47:11 +0100

    introducing a result component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
+import ReactPlayer from 'react-player';
+
+import {useResultContext} from './contexts/ResultContextProvider';
+import {Loading} from './Loading';
 
 export const Results = () => {
+    const {results, isLoading, getResults, searchTerm} = useResultContext();
+    const location = useLocation();
+
+    if(isLoading) return <Loading />
+
     return (
         <div>
             
